9afc97993a14adec320990c013b5f39626d97a70
Gokul-Js <sharma.gokul258@gmail.com>
2022-03-13 23:28:10 +0530

    Till emailjs

---
+++ youtube/src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://pluspng.com/img-png/png-development-modules-development-460.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "https://inextrix.com/wp-content/uploads/2019/09/Web_development-4.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "https://www.pngmart.com/files/7/SEO-Transparent-PNG.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "https://freepngimg.com/download/web_design/12-2-web-design-png-images.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "https://pluspng.com/img-png/png-development-modules-development-460.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "https://cdni.iconscout.com/illustration/premium/thumb/freelancer-working-on-project-3839580-3202882.png",
+      link: "http://lama.dev",
+    },
+  ];
